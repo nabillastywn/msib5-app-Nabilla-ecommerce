2d80ba2edc4d42f3679e0e8dbaad876932b03ba4
nabillastywn <116862851+nabillastywn@users.noreply.github.com>
2023-09-15 21:29:30 +0700

    first-commit

--- pages/index.vue
+++ pages/index.vue
@@ -51,10 +51,28 @@
           <img src="../assets/img/Button.png" alt="Arrow Right" width="18" />
         </div>
       </div>
-      <div class="grid grid-cols-5 gap-4 ml-20 mr-24">
-        <div v-for="(p, index) in products" :key="p.id">
-          <FlashsaleCard :product="p" v-if="index < 5" />
-        </div>
+      <div class="mx-24">
+        <Swiper
+          :modules="modules"
+          :slides-per-view="5"
+          :navigation="{ enabled: true, nextEl: '.button-next' }"
+          :space-between="10"
+          :loop="true"
+          :autoplay="{
+            delay: 7000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          }"
+        >
+          <SwiperSlide v-for="p in products">
+            <FlashsaleCard :product="p" />
+          </SwiperSlide>
+          <div
+            class="button-next w-7 h-7 bg-gray-400 absolute z-10 right-0 top-[155px] rounded-full mr-[1px] mb-12"
+          >
+            <img src="../assets/img/flash/Group.svg" />
+          </div>
+        </Swiper>
       </div>
     </div>
     <div class="mx-24 px-31 flex justify-between mb-10 mt-24 items-center">
@@ -78,10 +96,28 @@
         <div class="text-5xl font-bold text-yellow-300 mt-2 mb-3">50%</div>
       </div>
     </div>
-    <div class="grid grid-cols-4 gap-4 ml-72 mr-24 mt-[-355px]">
-      <div v-for="(p, index) in products" :key="p.id">
-        <LatestCard :product="p" v-if="index < 4" />
-      </div>
+    <div class="ml-72 mr-24 mt-[-355px]">
+      <Swiper
+        :modules="modules"
+        :slides-per-view="4"
+        :navigation="{ enabled: true, nextEl: '.button-next' }"
+        :space-between="10"
+        :loop="true"
+        :autoplay="{
+          delay: 7000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }"
+      >
+        <SwiperSlide v-for="p in products">
+          <LatestCard :product="p" />
+        </SwiperSlide>
+        <div
+          class="button-next w-7 h-7 bg-gray-400 absolute z-10 right-0 top-[155px] rounded-full mr-[1px] mb-12"
+        >
+          <img src="../assets/img/flash/Group.svg" />
+        </div>
+      </Swiper>
     </div>
     <div class="mx-24 px-31 flex mb-10 mt-24 items-center">
       <div class="font-bold text-secondary text-lg">Promo</div>
@@ -110,10 +146,28 @@
         <div class="text-5xl font-bold text-yellow-300 mt-2 mb-3">50%</div>
       </div>
     </div>
-    <div class="grid grid-cols-4 gap-4 ml-72 mr-24 mt-[-355px]">
-      <div v-for="(p, index) in products" :key="p.id">
-        <LatestCard :product="p" v-if="index < 4" />
-      </div>
+    <div class="ml-72 mr-24 mt-[-355px]">
+      <Swiper
+        :modules="modules"
+        :slides-per-view="4"
+        :navigation="{ enabled: true, nextEl: '.button-next' }"
+        :space-between="10"
+        :loop="true"
+        :autoplay="{
+          delay: 7000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }"
+      >
+        <SwiperSlide v-for="p in products">
+          <LatestCard :product="p" />
+        </SwiperSlide>
+        <div
+          class="button-next w-7 h-7 bg-gray-400 absolute z-10 right-0 top-[155px] rounded-full mr-[1px] mb-12"
+        >
+          <img src="../assets/img/flash/Group.svg" />
+        </div>
+      </Swiper>
     </div>
     <div class="mx-24 px-31 flex justify-between mb-10 mt-24 items-center">
       <div class="font-bold text-secondary text-lg">For You</div>
@@ -127,12 +181,36 @@
         <ProductCard :product="p" v-if="index < 15" />
       </div>
     </div>
+    <!-- contoh -->
+    <!-- <div class="w-96 bg-white rounded-xl border border-gray-200 p-4 flex">
+      <div class="w-40 bg-gray-100 rounded-lg">
+        <img class="w-full h-full" src="product.image" alt="Product Image" />
+      </div>
+      <div class="flex-grow flex flex-col justify-between ml-4">
+        <div>
+          <div class="text-gray-900 text-xl font-semibold leading-tight">
+            Product Name
+          </div>
+          <div class="text-gray-400 text-xs">Color Name</div>
+        </div>
+        <div class="text-slate-700 text-lg font-medium">
+          Qty: 1
+          <span class="text-red-700 text-sm font-semibold ml-2 cursor-pointer"
+            >Remove</span
+          >
+        </div>
+      </div>
+      <div class="text-gray-900 text-xl font-semibold ml-4">Rp36.000</div>
+    </div> -->
+    <!-- contoh -->
   </div>
 </template>
 
 <script setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation, Autoplay } from "swiper/modules";
+const modules = [Navigation, Autoplay];
 const { data: products } = await useFetch("https://fakestoreapi.com/products");
-
 definePageMeta({
   layout: "landing",
 });
